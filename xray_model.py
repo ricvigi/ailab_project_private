c293844e28b7d6fcb78397a6394fb4db6fe3d71f
ricvigi <ricvigi@proton.me>
2024-09-02 13:15:22 +0200

    modified xray_model.py

--- xray_model.py
+++ xray_model.py
@@ -1,30 +1,47 @@
-import torch, torchvision, datetime
+import torch, torchvision, datetime, os
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data as data
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from torchvision import transforms
 from PIL import Image, ImageFile
 
-################################################# FUNCTIONS #################################################À
+################################################# FUNCTIONS #################################################
+def train_val_split(data):
+    n_samples = len(datafolder)
+    n_val = int(0.3 * n_samples)
+
+    shuffled_indices = torch.randperm(n_samples)
+
+    train_indices = shuffled_indices[:-n_val]
+    val_indices = shuffled_indices[-n_val:]
+    print(train_indices.shape, val_indices.shape, sep="\n")
+
+    train_data = [datafolder[x] for x in train_indices]
+    assert len(train_data) == train_indices.shape[0], "[*] ERROR: Something went wrong in splitting data (train data)"
+    val_data = [datafolder[x] for x in val_indices]
+    assert len(val_data) == val_indices.shape[0], "[*] ERROR: Something went wrong in splitting data (val data)"
+
+    return train_data, val_data
+
 def validate(model, val_loader) -> None:
-    for name, loader in [("Knife", val_loader), ("Scissor", val_loader), ("camera", val_loader), ("cellphone", val_loader), ("electronic", val_loader), ("laptop", val_loader), ("lighter", val_loader), ("powerbank", val_loader)]:
+    model.eval()
+    for name, loader in [("knife", val_loader), ("scissor", val_loader), ("camera", val_loader), ("cellphone", val_loader), ("electronic", val_loader), ("laptop", val_loader), ("lighter", val_loader), ("powerbank", val_loader)]:
         correct = 0
         total = 0
 
         with torch.no_grad():  # <1>
             for imgs, labels in loader:
                 outputs = model(imgs)
-                _, predicted = torch.max(outputs, dim=1) # <2>
-                total += labels.shape[0]  # <3>
-                correct += int((predicted == labels).sum())  # <4>
+                _, predicted = torch.max(outputs, dim=1)
+                total += labels.shape[0]
+                correct += int((predicted == labels).sum())
 
         print("Accuracy {}: {:.2f}".format(name , correct / total))
     return
 
-
 # training loop with l2 regularization
-def training_loop_l2reg(n_epochs, optimizer, model, loss_fn,
+def training_loop(n_epochs, optimizer, model, loss_fn,
                         train_loader) -> None:
     for epoch in range(1, n_epochs + 1):
         loss_train = 0.0
@@ -34,62 +51,60 @@ def training_loop_l2reg(n_epochs, optimizer, model, loss_fn,
             outputs = model(imgs)
             loss = loss_fn(outputs, labels)
 
-            # l2 regularization
-            # l2_lambda = 0.001
-            # l2_norm = sum(p.pow(2.0).sum()
-            #               for p in model.parameters())  # <1>
-            # loss = loss + l2_lambda * l2_norm
-
-            # zero out the gradient, propagate the loss, perform the optimization step
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             loss_train += loss.item()
-        if epoch == 1 or epoch % 10 == 0:
-            print('{} Epoch {}, Training loss {}'.format(
-                datetime.datetime.now(), epoch,
-                loss_train / len(train_loader)))
+        if epoch == 1 or epoch % 1 == 0:
+            print(f"{datetime.datetime.now()} Epoch {epoch}, Training loss {loss_train / len(train_loader)}")
     return
 
-# a useful function to show an image that takes a tensor as an input
+
+# useful function to show an image that takes a tensor as an input
 def show_img(t_img:torch.tensor) -> None:
-    assert t_img.shape[0] <= 3 , "You're trying to display a batch of size > 1. Be wary of opening more than one image..."
+    if len(t_img.shape > 3):
+        assert t_img.shape[0] <= 1, "You're trying to display a batch of size > 1. Be wary of opening more than one image at a time..."
     to_img = transforms.ToPILImage()
     img = to_img(t_img)
     img.show()
-    return 
+    return
 
-#################################### MODELS ####################################
-import torch, kernels
+#################################### MODEL ####################################
+import kernels
 
 class MyModel(nn.Module):
     def __init__(self, n_chans1=32):
         super(MyModel, self).__init__()
-        self.gaussian_blur = nn.Conv2d(3, 3, kernel_size=3, padding=1)  
-        self.sharpen_kernel = nn.Conv2d(3, 3, kernel_size=3, padding=1)
+
+        # data preprocessing
+        self.gaussian_blur = nn.Conv2d(3, 3, kernel_size=3, padding=1)
         self.edge_detection = nn.Conv2d(3, 3, kernel_size=3, padding=1)
-        
+
         self.n_chans1 = n_chans1
+
+        # first convolution and batch normalization
         self.conv1 = nn.Conv2d(3, n_chans1, kernel_size=3, padding=1)
         self.conv1_batchnorm = nn.BatchNorm2d(num_features=n_chans1)
-        self.conv2 = nn.Conv2d(n_chans1, n_chans1 // 2, kernel_size=3, 
-                               padding=1)
+
+        # second convolution and batch normalization
+        self.conv2 = nn.Conv2d(n_chans1, n_chans1 // 2, kernel_size=3, padding=1)
         self.conv2_batchnorm = nn.BatchNorm2d(num_features=n_chans1 // 2)
+
+        # linear layers
         self.fc1 = nn.Linear(2 * 8 * 8 * n_chans1, 32)
         self.fc2 = nn.Linear(32, 8)
-        
+
         with torch.no_grad():
             self.gaussian_blur.bias.zero_()
-            self.sharpen_kernel.bias.zero_()
             self.edge_detection.bias.zero_()
             self.gaussian_blur.weight[:] = kernels.gaussian_blur3x3
-            self.sharpen_kernel.weight[:] = kernels.sharpen_kernel
             self.edge_detection.weight[:] = kernels.edge_detection_RIDGE0
+
+    # forward pass
     def forward(self, x):
         out = self.gaussian_blur(x)
         out = self.edge_detection(out)
-        
         out = self.conv1_batchnorm(self.conv1(x))
         out = F.max_pool2d(torch.tanh(out), 2)
         out = self.conv2_batchnorm(self.conv2(out))
@@ -100,10 +115,15 @@ class MyModel(nn.Module):
         return out
 
 ####################################### GLOBAL VARIABLES ##########################################
-path = "/home/rick/Ri/SecondYear/2ndSemester/AI-Lab/project_private/temp_train"
+path = ""
+assert (path != ""), "[*]ERROR: You forgot to include the data path in the path variable"
 device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')) # train on gpu if available
-model_path = "/home/rick/Ri/SecondYear/2ndSemester/AI-Lab/project_private/"
-model_name = "model1.pt"
+model_path = ""
+assert (model_path != ""), "[*]ERROR: You forgot to include the model path in the model_path variable"
+model_name = ""
+assert (model_name != ""), "[*]Error: You forgot to include the model name in the model_name variable"
+
+
 ################################## SCRIPT #######################################
 # resize and transform images into tensors
 # normalization is done later, so we don't include it in the first transforms
@@ -111,46 +131,42 @@ transformations = transforms.Compose([
     transforms.Resize((64,64)),
     transforms.ToTensor()
 ])
+
+print(f"[*] LOADING IMAGES")
 datafolder = torchvision.datasets.ImageFolder(root=path, transform=transformations)
 datafolder.classes = [(x, c) for c, x in enumerate(datafolder.classes)]
-print(datafolder.classes)
-# create array of indices for training and validation set
-n_samples = len(datafolder)
-n_val = int(0.3 * n_samples)
+print(f"Classes: {datafolder.classes}")
 
-shuffled_indices = torch.randperm(n_samples)
-
-train_indices = shuffled_indices[:-n_val]
-val_indices = shuffled_indices[-n_val:]
-
-print(train_indices.shape, val_indices.shape, sep="\n")
-train_data = [datafolder[x] for x in train_indices]
-assert len(train_data) == train_indices.shape[0]
-val_data = [datafolder[x] for x in val_indices]
-assert len(val_data) == val_indices.shape[0]
+# split the data into training and validation data
+train_data, val_data = train_val_split(datafolder)
 
 # create the training and validation tensors
+print(f"[*] Creating training and validation tensors")
 train_imgs = {c:[] for c, x in enumerate(datafolder.classes)} # train_imgs.keys() are integers representing classes
 val_imgs = {c:[] for c, x in enumerate(datafolder.classes)}
+
+
 # extract images and labels from the DataLoader
+print(f"[*] Extracting images and labels from the DataLoader")
 for c, (img_t, label) in enumerate(train_data):
     if c % 1000 == 0:
-        print(f"processing img {c}")
+        print(f"Processing img {c}")
     train_imgs[label].append(img_t)
-    
+
 for c, (img_t, label) in enumerate(val_data):
     if c % 1000 == 0:
-        print(f"processing img {c}")
+        print(f"Processing img {c}")
     val_imgs[label].append(img_t)
 
 # stack the images into an extra dimension
+print(f"stacking the images into an extra dimension")
 for key in train_imgs:
     train_imgs[key] = torch.stack(train_imgs[key])
 
 for key in val_imgs:
     val_imgs[key] = torch.stack(val_imgs[key])
 
-# normalize the images belonging to each class! 
+# normalize the images belonging to each class!
 # NOTE: normalize for each class,
 # not over all the images
 
@@ -158,6 +174,7 @@ ntrain_imgs = []
 nval_imgs = []
 
 # normalize the training set
+print(f"[*] Normalizing the training set")
 class_means = {}
 class_std = {}
 
@@ -165,19 +182,21 @@ for key in train_imgs:
     class_means[key] = torch.mean(train_imgs[key], (0, 2, 3))
     class_std[key] = torch.std(train_imgs[key], (0, 2, 3))
     ntrain_imgs.append(((train_imgs[key] - class_means[key][None, :, None, None]) / class_std[key][None, :, None, None], key))
-    
+
 # normalize the validation set
+print(f"[*] Normalizing the validation set")
 class_means = {}
 class_std = {}
 
 for key in val_imgs:
     class_means[key] = torch.mean(val_imgs[key], (0, 2, 3))
-    class_std[key] = torch.std(train_imgs[key], (0, 2, 3))
+    class_std[key] = torch.std(val_imgs[key], (0, 2, 3))
+    #class_std[key] = torch.std(train_imgs[key], (0, 2, 3))
     nval_imgs.append(((val_imgs[key] - class_means[key][None, :, None, None]) / class_std[key][None, :, None, None], key))
-    
-# further preprocess necessary to perfom forward pass
-n_train_imgs = []
 
+# further preprocess necessary to perfom forward pass
+print(f"[*] Preprocessing for forward pass")
+n_train_imgs = []
 for label in range(len(ntrain_imgs)):
     for k in range(ntrain_imgs[label][0].shape[0]):
         n_train_imgs.append((ntrain_imgs[label][0][k], label))
@@ -189,7 +208,8 @@ for label in range(len(nval_imgs)):
         n_val_imgs.append((nval_imgs[label][0][k], label))
 print(len(n_val_imgs))
 
-# create loaders for training set and validation set. Also perform another shuffle
+# create loaders for training set and validation set.
+print(f"[*] Create loaders for training set and validation set")
 train_loader = torch.utils.data.DataLoader(n_train_imgs, batch_size=64, shuffle=True)
 val_loader = torch.utils.data.DataLoader(n_val_imgs, batch_size=64, shuffle=True)
 
@@ -203,27 +223,34 @@ for imt_t, label in val_loader:
 n_out = len(datafolder.classes)
 print(f"[*] Dataset contains {n_out} classes)")
 
-numel_list = [p.numel() for p in model.parameters()]
-print("[*]Number of parameters:", sum(numel_list), numel_list)
-
-
-
 model = MyModel()
 
-optimizer = optim.SGD(model.parameters(), lr=.6e-2, weight_decay=1e-3) # weight_decay acts like l2 regularization  
-loss_fn = nn.CrossEntropyLoss()  
-n_epochs = 20
+
+# load previously trained state dict
+if os.path.exists(model_path + model_name):
+    print(f"[*] Resuming training. Loading previous state dict")
+    model.load_state_dict(torch.load(model_path + model_name))
+
+numel_list = [p.numel() for p in model.parameters()]
+print("[*] Number of parameters:", sum(numel_list), numel_list)
+
+
+optimizer = optim.SGD(model.parameters(), lr=.6e-2, weight_decay=1e-3) # NOTE: weight_decay acts like l2 regularization
+loss_fn = nn.CrossEntropyLoss()
+n_epochs = 60
 
 # train the model
-# training_loop_l2reg(  
-#     n_epochs = n_epochs,
-#     optimizer = optimizer,
-#     model = model,
-#     loss_fn = loss_fn,
-#     train_loader = train_loader,
-# )
+print(f"[*] TRAINING for {n_epochs} epochs")
+training_loop(
+     n_epochs = n_epochs,
+     optimizer = optimizer,
+     model = model,
+     loss_fn = loss_fn,
+     train_loader = train_loader,
+    )
 
 # validation
-# validate(model, val_loader)
+validate(model, val_loader)
 
-# torch.save(model.state_dict(), model_path + model_name) 
+# update the model's state dictionary
+torch.save(model.state_dict(), model_path + model_name)
